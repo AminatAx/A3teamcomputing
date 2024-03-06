eddde929e8ec665d370f79ce196b1bfb722a316f
BouthaynaM <158314753+BouthaynaM@users.noreply.github.com>
2024-03-06 11:06:45 +0000

    Add files via upload

---
+++ Lab1/ABLab1-looper-RobotC (1).c
@@ -0,0 +1,18 @@
+#pragma config(Sensor, S1,     touchSensor,    sensorEV3_Touch)
+#pragma config(Sensor, S2,     gyroSensor,     sensorEV3_Gyro, modeEV3Gyro_RateAndAngle)
+#pragma config(Sensor, S3,     colorSensor,    sensorEV3_Color, modeEV3Color_Color)
+#pragma config(Sensor, S4,     sonarSensor,    sensorEV3_Ultrasonic)
+#pragma config(Motor,  motorA,          armMotor,      tmotorEV3_Large, PIDControl, encoder)
+#pragma config(Motor,  motorB,          leftMotor,     tmotorEV3_Large, PIDControl, driveLeft, encoder)
+#pragma config(Motor,  motorC,          rightMotor,    tmotorEV3_Large, PIDControl, driveRight, encoder)
+
+
+task main()
+{		
+		//setting the motors to the correct speed to loop
+		setMotorSpeed(leftMotor, 60);
+		setMotorSpeed(rightMotor, 100);
+		sleep(7000);
+
+
+}
