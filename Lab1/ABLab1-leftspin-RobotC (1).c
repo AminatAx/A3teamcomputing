5d70b9aae202b811544706781c98f32b292b7742
BouthaynaM <158314753+BouthaynaM@users.noreply.github.com>
2024-03-06 11:08:24 +0000

    Add files via upload

---
+++ Lab1/ABLab1-leftspin-RobotC (1).c
@@ -0,0 +1,22 @@
+#pragma config(Sensor, S1,     touchSensor,    sensorEV3_Touch)
+#pragma config(Sensor, S2,     gyroSensor,     sensorEV3_Gyro, modeEV3Gyro_RateAndAngle)
+#pragma config(Sensor, S3,     colorSensor,    sensorEV3_Color, modeEV3Color_Color)
+#pragma config(Sensor, S4,     sonarSensor,    sensorEV3_Ultrasonic)
+#pragma config(Motor,  motorA,          armMotor,      tmotorEV3_Large, PIDControl, encoder)
+#pragma config(Motor,  motorB,          leftMotor,     tmotorEV3_Large, PIDControl, driveLeft, encoder)
+#pragma config(Motor,  motorC,          rightMotor,    tmotorEV3_Large, PIDControl, driveRight, encoder)
+
+
+task main()
+{
+		//moving forward 50 centimeters
+		setMotorSpeed(leftMotor, 50);
+		setMotorSpeed(rightMotor, 50);
+		sleep(2000);
+
+		//doing a 360 spin
+		setMotorSpeed(leftMotor, 50);
+		setMotorSpeed(rightMotor, 0);
+		sleep(3100);
+
+}
