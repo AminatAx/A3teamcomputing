1da387d10e4d7083b6a82ccb8e54ed94fc0ae700
AminatAx <158291152+AminatAx@users.noreply.github.com>
2024-03-13 12:57:41 +0000

    Update ABteamcomputinglab2PIDSYNC (1).c

--- Lab2/ABteamcomputinglab2PIDSYNC (1).c
+++ Lab2/ABteamcomputinglab2PIDSYNC (1).c
@@ -7,8 +7,9 @@
 #pragma config(Motor,  motorC,          rightMotor,    tmotorEV3_Large, PIDControl, driveRight, encoder)
 //setMotorSyncEncoder(nMotorOne, nMotorTwo, nTurnRatio, nEncoderCount, nSignedPower);
 
+//Programme description: Using PID controls create 2 functions: moveRobot and randomDirection
 
-
+//Function to get the robot to move forward
 void moveRobot(long nMotorRatio, long time, long power)
 {
 	setMotorSyncTime(leftMotor, rightMotor, nMotorRatio, time, power); // part 5a getting robot to move forward
@@ -16,6 +17,7 @@ void moveRobot(long nMotorRatio, long time, long power)
 
 }
 
+//if statement that set the robot to move in a random direction depending on if it chooses a 0 or 1
 void randomDirection()
 {
 	int Direction = random(1);
@@ -30,13 +32,14 @@ void randomDirection()
 
 }
 
-
+//main function that calls the moveRobot and randomDirection and imlements the code
 task main()
 {
 	moveRobot(0, 3000, 50);
 
 	randomDirection();
 
+//this part of the code implement Lab2 part 6 which gets the robot to move in a square and to go forward for 2 second  at full speed, spin 180 degrees and then travel back at half speed
 	moveRobot(0, 3000, 50);
 	moveRobot(100, 500, 50);
 	moveRobot(0, 3000, 50);
